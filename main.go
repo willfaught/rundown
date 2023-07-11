caea56acc2fe036c434a47b38fd55a09f6fd43e2
Will Faught <willfaught@users.noreply.github.com>
2023-07-10 21:19:01 -0700

    Add usage to package doc

--- main.go
+++ main.go
@@ -1,4 +1,11 @@
 // Package rundown prints package documentation as Markdown.
+//
+// # Usage
+/*
+	Usage of rundown:
+	  -package string
+	    	Package path (default ".")
+*/
 package main
 
 import (
